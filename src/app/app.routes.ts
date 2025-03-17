56c7753a0b8528b8647c0e4f4163de90b14cab9b
VictorBartolomeo <victor.monteriggioni@gmail.com>
2025-03-17 22:51:10 +0100

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from './pages/home/home.component';
+import {LoginComponent} from './pages/login/login.component';
+import {NotfoundComponent} from './pages/notfound/notfound.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path : "home", component : HomeComponent},
+  {path : "login", component : LoginComponent},
+  {path : "", redirectTo :"home", pathMatch :"full"},
+  {path : "**", component : NotfoundComponent},
+];
