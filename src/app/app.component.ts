e157a7cf44a696e72b6664d809179834532c3e2b
VictorBartolomeo <victor.monteriggioni@gmail.com>
2025-03-21 15:40:02 +0100

    Add imports for menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import {HomeComponent} from './pages/home/home.component';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+// import {MatButton} from '@angular/material/menu';
+import {MenuComponent} from './components/menu/menu.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HomeComponent],
+  imports: [RouterOutlet,
+    MenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'angular_java';
+  title = '🍍Pineapple Store';
 }
