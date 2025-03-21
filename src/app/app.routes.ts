d4764d191d4cfdaa0d85fc72051df9a0983f1ca4
VictorBartolomeo <victor.monteriggioni@gmail.com>
2025-03-21 15:39:23 +0100

    refacto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
-import {HomeComponent} from './pages/home/home.component';
-import {LoginComponent} from './pages/login/login.component';
-import {NotfoundComponent} from './pages/notfound/notfound.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { NotfoundComponent } from './pages/notfound/notfound.component';
 
 export const routes: Routes = [
   {path : "home", component : HomeComponent},
