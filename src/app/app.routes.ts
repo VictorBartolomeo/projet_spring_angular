d8dcfbe4ff01a8a53b23c3d1d9fc5b0cf6e18b90
VictorBartolomeo <victor.monteriggioni@gmail.com>
2025-04-01 08:42:08 +0200

    Add route for splitting edit and add

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,14 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { NotfoundComponent } from './pages/notfound/notfound.component';
+import {EditProductComponent} from './pages/edit-product/edit-product.component';
 
 export const routes: Routes = [
   {path : "home", component : HomeComponent},
   {path : "login", component : LoginComponent},
+  {path : "add-product", component : EditProductComponent},
+  {path : "edit-product/:id", component : EditProductComponent},
   {path : "", redirectTo :"home", pathMatch :"full"},
   {path : "**", component : NotfoundComponent},
 ];
+
