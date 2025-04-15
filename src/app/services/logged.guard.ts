7ff7a96f12367af5a03830bd124243ecd984ac71
VictorBartolomeo <victor.monteriggioni@gmail.com>
2025-04-15 13:44:42 +0200

    create guard service

---
+++ src/app/services/logged.guard.ts
@@ -0,0 +1,14 @@
+import {CanActivateFn, Router} from '@angular/router';
+import {inject} from '@angular/core';
+
+export const loggedGuard: CanActivateFn = (route, state) => {
+
+  const jwt = localStorage.getItem('jwt');
+
+  if (jwt === null) {
+    const router: Router = inject(Router);
+    return router.parseUrl('/login')
+  }
+
+  return true;
+};
