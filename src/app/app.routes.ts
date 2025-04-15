22a56e1225a0babb40d1622a259d10849d082a26
VictorBartolomeo <victor.monteriggioni@gmail.com>
2025-04-15 13:46:41 +0200

    adding loggedGuard to constraint pages access

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,16 @@
-import { Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
-import { LoginComponent } from './pages/login/login.component';
-import { NotfoundComponent } from './pages/notfound/notfound.component';
+import {Routes} from '@angular/router';
+import {HomeComponent} from './pages/home/home.component';
+import {LoginComponent} from './pages/login/login.component';
+import {NotfoundComponent} from './pages/notfound/notfound.component';
 import {EditProductComponent} from './pages/edit-product/edit-product.component';
+import {loggedGuard} from './services/logged.guard';
 
 export const routes: Routes = [
-  {path : "home", component : HomeComponent},
-  {path : "login", component : LoginComponent},
-  {path : "add-product", component : EditProductComponent},
-  {path : "edit-product/:id", component : EditProductComponent},
-  {path : "", redirectTo :"home", pathMatch :"full"},
-  {path : "**", component : NotfoundComponent},
+  {path: "home", component: HomeComponent, canActivate: [loggedGuard]},
+  {path: "login", component: LoginComponent},
+  {path: "add-product", component: EditProductComponent, canActivate: [loggedGuard]},
+  {path: "edit-product/:id", component: EditProductComponent, canActivate: [loggedGuard]},
+  {path: "", redirectTo: "home", pathMatch: "full"},
+  {path: "**", component: NotfoundComponent},
 ];
 
