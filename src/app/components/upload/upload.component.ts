3dbbad56ab2491aee2194b1ef3aa55ea99566b10
VictorBartolomeo <victor.monteriggioni@gmail.com>
2025-05-12 09:52:43 +0200

    Add upload component with file selection and styling functionality.

---
+++ src/app/components/upload/upload.component.ts
@@ -0,0 +1,27 @@
+import {Component, EventEmitter, Output} from '@angular/core';
+import {MatIcon} from '@angular/material/icon';
+import {FormsModule} from '@angular/forms';
+
+@Component({
+  selector: 'app-upload',
+  imports: [
+    MatIcon, FormsModule
+  ],
+  templateUrl: './upload.component.html',
+  styleUrl: './upload.component.scss'
+})
+export class UploadComponent {
+
+  file?: File;
+
+  @Output()
+  selectedFile= new EventEmitter<File |null>();
+
+
+  onFileChange(event:any) {
+    this.file = event.target.files[0];
+    this.selectedFile.emit(this.file);
+  }
+
+}
+
