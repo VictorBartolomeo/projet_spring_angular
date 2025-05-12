ba2c450631143b86fc4faaaa378cdc8366263106
VictorBartolomeo <victor.monteriggioni@gmail.com>
2025-05-12 09:03:26 +0200

    "Remove unused slide toggles and center menu content layout."

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -2,12 +2,11 @@ import {Component, inject} from '@angular/core';
 import {MatMenuModule} from '@angular/material/menu';
 import {RouterLink, RouterLinkActive} from '@angular/router';
 import {MatButton} from '@angular/material/button';
-import {MatSlideToggle} from '@angular/material/slide-toggle';
 import {AuthService} from '../../services/auth.service';
 
 @Component({
   selector: 'app-menu',
-  imports: [MatMenuModule, RouterLink, RouterLinkActive, MatButton, MatSlideToggle],
+  imports: [MatMenuModule, RouterLink, RouterLinkActive, MatButton],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.scss'
 })
