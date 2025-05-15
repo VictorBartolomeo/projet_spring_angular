eec46069050ea73effad3a58076198dd77989e7e
VictorBartolomeo <victor.monteriggioni@gmail.com>
2025-05-15 09:02:40 +0200

    Add SecuredImgDirective with basic implementation and unit test

---
+++ src/app/components/secured-img.directive.spec.ts
@@ -0,0 +1,8 @@
+import { SecuredImgDirective } from './secured-img.directive';
+
+describe('SecuredImgDirective', () => {
+  it('should create an instance', () => {
+    const directive = new SecuredImgDirective();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ src/app/components/secured-img.directive.ts
@@ -0,0 +1,10 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appSecuredImg]'
+})
+export class SecuredImgDirective {
+
+  constructor() { }
+
+}
