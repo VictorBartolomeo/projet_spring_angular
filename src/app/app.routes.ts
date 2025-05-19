17e22d3b2b0c8e06a2722b829ffa03c96d34e05d
VictorBartolomeo <victor.monteriggioni@gmail.com>
2025-05-19 09:36:56 +0200

    Add "validate-mail/:token" route to app routes.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -11,6 +11,7 @@ export const routes: Routes = [
   {path: "login", component: LoginComponent},
   {path: "add-product", component: EditProductComponent, canActivate: [sellerGuard]},
   {path: "edit-product/:id", component: EditProductComponent, canActivate: [sellerGuard]},
+  {path: "validate-mail/:token", component: HomeComponent},
   {path: "", redirectTo: "home", pathMatch: "full"},
   {path: "**", component: NotfoundComponent},
 ];
