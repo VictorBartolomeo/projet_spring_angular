706d35bd540a692aefa8d3bb0c9a7094b2dfdac9
VictorBartolomeo <victor.monteriggioni@gmail.com>
2025-05-19 09:37:40 +0200

    Add ValidateMailComponent with initial setup and tests

---
+++ src/app/pages/validate-mail/validate-mail.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-validate-mail',
+  imports: [],
+  templateUrl: './validate-mail.component.html',
+  styleUrl: './validate-mail.component.scss'
+})
+export class ValidateMailComponent {
+
+}
