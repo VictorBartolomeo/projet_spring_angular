29b13093fa8e02d11990df3387d0717f1463be8b
VictorBartolomeo <victor.monteriggioni@gmail.com>
2025-05-19 10:38:04 +0200

    Update validate-mail route to use ValidateMailComponent instead of HomeComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,13 +5,14 @@ import {NotfoundComponent} from './pages/notfound/notfound.component';
 import {EditProductComponent} from './pages/edit-product/edit-product.component';
 import {loggedGuard} from './services/logged.guard';
 import {sellerGuard} from './services/seller.guard';
+import {ValidateMailComponent} from './pages/validate-mail/validate-mail.component';
 
 export const routes: Routes = [
   {path: "home", component: HomeComponent, canActivate: [loggedGuard]},
   {path: "login", component: LoginComponent},
   {path: "add-product", component: EditProductComponent, canActivate: [sellerGuard]},
   {path: "edit-product/:id", component: EditProductComponent, canActivate: [sellerGuard]},
-  {path: "validate-mail/:token", component: HomeComponent},
+  {path: "validate-mail/:token", component: ValidateMailComponent},
   {path: "", redirectTo: "home", pathMatch: "full"},
   {path: "**", component: NotfoundComponent},
 ];
