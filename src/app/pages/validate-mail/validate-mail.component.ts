ff8ed09bc80750b6b0cdf7be409ffbce90e3a3e7
VictorBartolomeo <victor.monteriggioni@gmail.com>
2025-05-19 10:39:59 +0200

    Add email validation logic with token handling and request integration in ValidateMailComponent.

--- src/app/pages/validate-mail/validate-mail.component.ts
+++ src/app/pages/validate-mail/validate-mail.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {ActivatedRoute, Router} from '@angular/router';
+import {NotificationService} from '../../services/notification.service';
 
 @Component({
   selector: 'app-validate-mail',
@@ -6,6 +9,30 @@ import { Component } from '@angular/core';
   templateUrl: './validate-mail.component.html',
   styleUrl: './validate-mail.component.scss'
 })
-export class ValidateMailComponent {
+export class ValidateMailComponent implements OnInit {
+  activatedRoute = inject(ActivatedRoute);
+  http = inject(HttpClient)
+  notification = inject(NotificationService)
+  router = inject(Router)
+  token?: string;
 
-}
+  ngOnInit() {
+    this.activatedRoute.params.subscribe(parameter => {
+      if (parameter['token']) {
+        this.token = parameter['token']
+      }
+    });
+  }
+
+  onValidateRegister() {
+    if(this.token){
+          this.http.post<{ token: string, CGU: boolean }>('http://localhost:8080/validate-email/' +this.token, {token : this.token, CGU: true})
+            .subscribe({
+              next : (result) => {
+                this.notification.showTop("Email validated", "valid")
+                this.router.navigateByUrl("/login")
+              }}
+            )
+        }
+      }
+    }
